2e6e70ed6dfb7714716149131094adba09f864d8
TapasyaTembhurney <tapasyatembhurney@gmail.com>
2024-02-27 09:41:06 +0530

    rmvaxiosimprt

--- src/useEffect/FakeApi.jsx
+++ src/useEffect/FakeApi.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 import BlogList from './BlogList'
-import axios from 'axios'
 function FakeApi() {
     const [blog,setBlog]= useState(null)
     useEffect(()=>{
